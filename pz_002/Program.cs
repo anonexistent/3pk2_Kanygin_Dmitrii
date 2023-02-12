7ea30e5ea4a50b3036d3e9c368117cfc45e784c3
Beekeepers <wwwzlx86@gmail.com>
2023-02-12 21:52:12 +0500

    победа над первым заданием

--- pz_002/Program.cs
+++ pz_002/Program.cs
@@ -14,11 +14,16 @@ namespace graph2
            Console.ResetColor();
         }
 
+        public static void Dijkstra(Graph g)
+        {
+
+        }
+
         public static void DFS(Graph graph, int node)
         {
            graph.Vector[node] = true; // отметить вершину i как обработанную
            Console.Write("{0}" + ' ', node); // распечатать номер посещенной вершины
-           for (int k = 0; k < graph.Size; k++)
+           for (int k = graph.Size-1; k >= 0; k--)
            {
                 if (graph.Adjacency[node, k] && !(graph.Vector[k])) DFS(graph,k);
            }
@@ -68,14 +73,37 @@ namespace graph2
                                         {
                                         {false, true, false, true}, // матрица смежности графа G4
                                         {true, false, false, false},
-                                        {true, true, false, true},
+                                        {true, true, false, true},      //  неполносвяз
                                         {false, true, false, false}
                                         };
             Graph graph = new Graph(4, M);
             //graph.Depth(2);
             DFS(graph, 0);
 
+            Console.WriteLine("\n——————————");
 
+            bool[,] N = new bool[4, 4]
+                                        {
+                                            {false, true, true, false}, // матрица смежности графа G41
+                                            {true, false, true, true},
+                                            {true, true, false, false},
+                                            {false, true, false, false}
+                                        };
+            Graph graph2 = new Graph(4, N);
+            DFS(graph2, 3);
+
+            Console.WriteLine("\n——————————");
+
+            bool[,] O = new bool[5, 5]
+                            {
+                                            {false, true, false,true,false}, // матрица смежности графа G5
+                                            {true,false,true,false,true},
+                                            {false,true,false,true,false},
+                                            {true,false,true,false,true},
+                                            {false,true,false,true,false}
+                            };
+            Graph graph3 = new Graph(5, O);
+            DFS(graph3, 2);
 
 
 
