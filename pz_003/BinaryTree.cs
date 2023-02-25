529deb442732571fa1be388f4a6ed99f28c87bcd
Beekeepers <wwwzlx86@gmail.com>
2023-02-25 14:59:57 +0500

    +node+bitre

---
+++ pz_003/BinaryTree.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_003
+{
+    internal class BinaryTree
+    {
+		private TreeNode root;
+
+		public TreeNode Root
+		{
+			get { return root; }
+			set { root = value; }
+		}
+		public BinaryTree()
+		{
+			Root = null;
+		}
+	}
+}
