8430fa65caafa88aa707f9da90e5549bb5ae7c75
Beekeepers <wwwzlx86@gmail.com>
2023-03-21 21:50:01 +0500

    start

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SolidMoment
+namespace pz_008
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SolidMoment
+namespace pz_008
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
