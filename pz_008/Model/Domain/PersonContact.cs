832dfe7362a8359217a7418c19073dec8fa27401
Дмитрий Каныгин <wwwzlx86@gmail.com>
2023-04-04 08:35:33 +0500

    разработка ги

---
+++ pz_008/Model/Domain/PersonContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_008.Model.Domain
+{
+    internal class PersonContact : IContact
+    {
+        public string Name { get; set; }
+        public long Phone {get; set;}
+        public string Region { get; set;}
+
+        public string[] GetInfo()
+        {
+            return new string[3] {Name, Phone.ToString(), Region};
+        }
+    }
+}
