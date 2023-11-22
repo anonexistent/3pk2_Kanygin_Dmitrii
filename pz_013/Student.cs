167678a3e768f71e8ef821ef7b7459d80e4df626
anonexistent <wwwzlx86@gmail.com>
2023-11-22 18:48:30 +0500

    pz 13 in work!!

---
+++ pz_013/Student.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pz_013
+{
+    internal class Student
+    {
+		[Required(ErrorMessage = "empty field")]
+		[StringLength(10, MinimumLength =1, ErrorMessage = "incorrect length")]
+		private string login;
+		public string Login
+		{
+			get { return login; }
+			set { login = value; }
+		}
+
+        [Required(ErrorMessage = "empty field")]
+        private string email;
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        [Required(ErrorMessage = "empty field")]
+        private string password;
+		public string Password
+		{
+			get { return password; }
+			set { password = value; }
+		}
+
+
+	}
+}
